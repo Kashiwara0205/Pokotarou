898816f5132830fd6eff9f5b34c5d21f40ce9090
Kashiwara <kashiwara0205@tekitou.com>
2019-08-01 11:18:07 +0900

    Modified get_foreign_key_data method
    for check wheter class exists

--- lib/pokotarou/data_structure.rb
+++ lib/pokotarou/data_structure.rb
@@ -27,11 +27,11 @@ class DataStructure
     def set_col_type config_data, str_model
       model = eval(str_model)
       foreign_key_data = get_foreign_key_data(model)
-      
-      config_data[:col] ||= Hash.new    
+
+      config_data[:col] ||= Hash.new
       model.columns.each do |e|
         symbol_col_name = e.name.to_sym
-        
+
         unless exists_seed_data?(config_data, symbol_col_name)
           # prepare setting to run default seed
           # set nil to seed data
@@ -44,7 +44,7 @@ class DataStructure
           # set enum info
           config_data[:enum] ||= Hash.new
           if is_enum?(e.sql_type.to_s)
-            config_data[:enum][symbol_col_name] = 
+            config_data[:enum][symbol_col_name] =
               e.sql_type.to_s[5..-2].tr("'", "").split(",")
           end
 
@@ -54,7 +54,7 @@ class DataStructure
             config_data[:foreign_key][symbol_col_name] = foreign_key_data[symbol_col_name]
           end
         end
-      
+
       end
     end
 
@@ -62,7 +62,10 @@ class DataStructure
       associations = model.reflect_on_all_associations(:belongs_to)
       return { } if associations.empty?
       associations.reduce(Hash.new)do |acc, r|
-        acc[r.foreign_key.to_sym] = eval(r.name.to_s.classify)
+        model = r.name.to_s.classify
+        if Object.const_defined?(model.to_sym)
+          acc[r.foreign_key.to_sym] = eval(model)
+        end
 
         acc
       end
@@ -75,11 +78,11 @@ class DataStructure
     def exists_seed_data? config_data ,symbol_col_name
       config_data[:col].has_key?(symbol_col_name)
     end
-    
+
     ENUM = /^enum(\s*.)*$/
     def is_enum? val
       return false unless val.kind_of?(String)
-      ENUM =~ val 
+      ENUM =~ val
     end
   end
 end
