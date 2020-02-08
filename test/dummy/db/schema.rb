4f436a7019dc194c672cab835dff98b0fbca0360
KASHIWARA FUKI <tanakatarou0205@yahoo.co.jp>
2020-02-08 17:45:04 +0900

    Update .gitignore

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -2,17 +2,17 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190630084138) do
+ActiveRecord::Schema.define(version: 2019_06_30_084138) do
 
-  create_table "members", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "members", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.text "remarks"
     t.date "birthday"
@@ -22,17 +22,17 @@ ActiveRecord::Schema.define(version: 20190630084138) do
     t.index ["pref_id"], name: "index_members_on_pref_id"
   end
 
-  create_table "prefs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "prefs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "test_models", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "test_models", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "string_test"
     t.text "text_test"
     t.integer "integer_test"
-    t.float "float_test", limit: 24
+    t.float "float_test"
     t.decimal "decimal_test", precision: 10
     t.datetime "datetime_test"
     t.timestamp "timestamp_test"
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 20190630084138) do
     t.boolean "boolean_test"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "enum_test", limit: 5, null: false
+    t.column "enum_test", "enum('enum1','enum2','enum3')", null: false
   end
 
 end
