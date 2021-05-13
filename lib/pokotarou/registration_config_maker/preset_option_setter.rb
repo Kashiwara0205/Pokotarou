b03546de7b3e578ffcfe81fd283513e86dc96110
kashiwara <tanakatarou0205@gmail.com>
2021-05-13 22:54:09 +0900

    複数のpreset展開に対応

--- lib/pokotarou/registration_config_maker/preset_option_setter.rb
+++ lib/pokotarou/registration_config_maker/preset_option_setter.rb
@@ -5,16 +5,24 @@ module Pokotarou
       class << self
         def set all_content
           preset_content = {}
-
+          merged_preset_path = []
           all_content[:"preset_path'"].each do |path|
             YAML.load_file(path).deep_symbolize_keys!.each do |preset|
+              if :"preset_path'" == preset.first
+                merged_preset_path.concat(preset.second)
+              else
                 preset_content[preset.first] = preset.second
+              end
             end
           end
 
-          # 使用済みのKeyなので削除
+          # preset_pathを更新
           all_content.delete(:"preset_path'")
-          
+          if merged_preset_path.size != 0
+            merged_preset_path.uniq!
+            all_content[:"preset_path'"] = merged_preset_path
+          end
+
           # hashの順番を更新するために一度全てのKeyを消す
           tmp = {}
           all_content.each do |config|
@@ -28,9 +36,17 @@ module Pokotarou
           # presetを先に展開する必要があるので、再代入して順番を変更する
           merge(all_content, preset_content)
           merge(all_content, tmp)
+
+          # presetを展開してpresetが入っていた場合には再度展開する
+          if all_content.has_key?(:"preset_path'")
+            set(all_content)
+          end
+
+          all_content
         end
 
         private
+
         def merge all_content, merge_hash
           merge_hash.each do |config|
             key = config.first
--- test/preset_test.rb
+++ test/preset_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class Pokotarou::PresetTest < ActiveSupport::TestCase  
+
   # outline: should works preset function
   # expected value: registerd 3 pref data 
   #                 registerd 1 member data
@@ -31,5 +32,32 @@ class Pokotarou::PresetTest < ActiveSupport::TestCase
     assert Member.find_by(name: "hoge").present?
     assert Member.find_by(name: "PresetMember1").present?
     assert Member.find_by(name: "PresetMember2").present?
+  end
+
+  # outline: presetのみでも動作することを担保する
+  # expected value: registerd 3 pref data 
+  test "make sure only preset config" do
+    Pokotarou.execute("test/data/preset/only_preset.yml")
+
+    assert_equal 3, Pref.all.count
+    assert Pref.find_by(name: "北海道").present?
+    assert Pref.find_by(name: "青森県").present?
+    assert Pref.find_by(name: "岩手県").present?
+  end
+
+  # outline: presetにpresetを使っているymlファイルがあっても動作することを担保する
+  # expected value: registerd 3 pref data 
+  test "make sure call preset config file in preset function" do
+    Pokotarou.execute("test/data/preset/preset_from_preset.yml")
+
+    assert_equal 3, Pref.all.count
+    assert Pref.find_by(name: "北海道").present?
+    assert Pref.find_by(name: "青森県").present?
+    assert Pref.find_by(name: "岩手県").present?
+
+    assert 1, Member.all.count
+    assert Member.find_by(name: "hoge").present?
+
+    assert_equal 3, TestModel.all.count
   end
 end
